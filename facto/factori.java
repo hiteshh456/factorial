79e915f7a19178d9c56789089f80bf83931fd756
hiteshh456 <91420226+hiteshh456@users.noreply.github.com>
2021-10-30 23:02:13 +0530

    Add files via upload
    factorial

---
+++ facto/factori.java
@@ -0,0 +1,17 @@
+package facto;
+
+public class factori {
+
+    public static void main(String[] args) {
+        int num = 10;
+        long factorial = multiplyNumbers(num);
+        System.out.println("Factorial of " + num + " = " + factorial);
+    }
+    public static long multiplyNumbers(int num)
+    {
+        if (num >= 1)
+            return num * multiplyNumbers(num - 1);
+        else
+            return 1;
+    }
+}
